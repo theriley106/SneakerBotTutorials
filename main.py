29767ded7db039f031ffad9579938f7ad168a9b6
Theriley106 <christopherlambert106@gmail.com>
2017-11-10 20:02:07 -0500

    Created temp solution for tommorrow

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import requests
 import bs4
 import re
 import selenium.webdriver
+import RandomHeaders
 import threading
+import sys
+URL = sys.argv[2]
+PROXIES = sys.argv[2:]
+SPLASHTITLE = grabCurrentTitle(URL)
 class bot(object):
 	def __init__(self, proxy):
 		print('Initiated')
@@ -13,22 +18,39 @@ class bot(object):
 		self.driver = webdriver.PhantomJS(service_args=['--proxy={}'.format(proxy), '--proxy-type=http'])
 		self.driver.get('https://www.reddit.com/r/cscareerquestions/')
 
-def grabScreenSize(proxy):
-	try:
-		headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-		driver = webdriver.PhantomJS(service_args=['--proxy={}'.format(proxy), '--proxy-type=https'])
-		#driver = webdriver.PhantomJS()
-		driver.set_page_load_timeout(10)
-		driver.get('https://www.privateinternetaccess.com/pages/whats-my-ip/')
-		driver.save_screenshot('{}.png'.format(proxy.replace(':', '').replace('.', '')))
-		driver.close()
-		driver.quit()
-	except Exception as exp:
-		print exp
+def grabCurrentTitle(url):
+	#this grabs the title of the splash page
+	driver = webdriver.PhantomJS()
+	driver.get(url)
+	title = driver.title
+	driver.close()
+	driver.quit()
+	return title
+
+def grabSS(proxy):
+	while True:
+		try:
+			headers = RandomHeaders.LoadHeader()
+			driver = webdriver.PhantomJS(service_args=['--proxy={}'.format(proxy), '--proxy-type=https'])
+			#driver = webdriver.PhantomJS()
+			driver.get(URL)
+			while driver.title == SPLASHTITLE:
+				driver.save_screenshot('{}.png'.format(proxy.replace(':', '').replace('.', '')))
+				#this just visualized the phantomjs driver - you can replace this with pass if you're trying to reduce mem
+			cookies_list = driver.get_cookies()
+			driver.close()
+			driver.quit()
+			driver = webdriver.Firefox()
+			for cookie in cookies_list:
+				driver.add_cookie(cookie)
+				#converts phantomjs cookies into firefox webdriver to check out
+			driver.get(URL)
+
+
+		except Exception as exp:
+			print exp
 if __name__ == "__main__":
-	r = requests.post("http://138.197.123.15:8888/proxies/{}".format(open('../../SecretCode.txt').read().strip())).json()
-	proxies = r["proxies"][:10]
-	threads = [threading.Thread(target=grabScreenSize, args=(proxy,)) for proxy in proxies]
+	threads = [threading.Thread(target=grabSS, args=(proxy,)) for proxy in PROXIES]
 	for thread in threads:
 		thread.start()
 	for thread in threads:
