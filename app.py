eb62ea1d6b440711e8bc4bd99167793609f9a2f7
Theriley106 <christopherlambert106@gmail.com>
2017-11-20 14:14:44 -0500

    Added commit scraper

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template, request, url_for, redirect, Markup, jsonify, make_response, send_from_directory, session
+import requests
+import bs4
+import RandomHeaders
+import re
 
 app = Flask(__name__)
 
+def getCommits():
+	try:
+		url = 'https://github.com/theriley106/SneakerBotTutorials'
+		res = requests.get(url, headers=RandomHeaders.LoadHeader())
+		page = bs4.BeautifulSoup(res.text)
+		commitsCount = page.select('.commits a')
+		return int(re.findall('\d+', str(commitsCount[0].getText()))[0])
+	except:
+		return "ERROR"
+
+
 @app.route('/', methods=['GET'])
 def index():
-	return render_template("index.html")
+	gitCommits = getCommits()
+	return render_template("index.html", gitCommits=gitCommits)
+
+
 
 if __name__ == '__main__':
-	print("this is working")
 	app.run(host='127.0.0.1', port=8000, debug=True)
